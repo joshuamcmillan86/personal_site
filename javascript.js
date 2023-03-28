897de4002712e5a67b286b5bf9836a2dcc045c41
joshuamcmillan86 <joshuamcmillan86@gmail.com>
2023-03-28 07:39:39 -0400

    finishing project touches

---
+++ javascript.js
@@ -0,0 +1,11 @@
+//Create a message welcoming to my personal website
+//Add the script tag
+
+const wel = 'Welcome'
+const t = 'to'
+const m = 'my'
+const p = 'personal'
+const w = 'website'
+
+alert(`${wel} ${t} ${m} ${p} ${w} !`)
+console.log('test')
